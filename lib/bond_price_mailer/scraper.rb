f09176e55af5e5a178bda66258410722c698b2ee
Jakob Kofoed Janot <jakob@janot.dk>
2015-04-28 09:41:50 +0200

    Xpath works again

--- lib/bond_price_mailer/scraper.rb
+++ lib/bond_price_mailer/scraper.rb
@@ -6,7 +6,7 @@ module BondPriceMailer
   Paper = Struct.new(:isin, :name, :close_price, :open_price,
                      :high_price, :low_price, :average_price, :link, :date) do
     def short
-      "#{name}: #{price}"
+      "#{name}: #{close_price}"
     end
 
     def long
@@ -31,28 +31,23 @@ module BondPriceMailer
 
   class Scraper
     def self.papers(instruments)
-      doc = bonds(instruments)
-      binding.pry
-      instruments.map do |instrument|
-        paper = REXML::XPath.first(doc, "inst[id/text() = '#{instrument}']")
-        Paper.new(
-          REXML::XPath.first(paper, './isin/text()').to_s,    # isin
-          REXML::XPath.first(paper, './nm/text()').to_s,      # name
-          REXML::XPath.first(paper, './cp/text()').to_s.to_f, # close_price
-          REXML::XPath.first(paper, './op/text()').to_s.to_f, # open_price
-          REXML::XPath.first(paper, './hp/text()').to_s.to_f, # high_price
-          REXML::XPath.first(paper, './lp/text()').to_s.to_f, # low_price
-          "http://www.nasdaqomxnordic.com/bonds/denmark/microsite?Instrument=#{instrument}",
+      papers = []
+      REXML::XPath.each(bonds(instruments), './/inst') do |inst|
+        id = inst.attributes['id']
+        papers << Paper.new(
+          *(inst.attributes.values_at('isin', 'nm', 'cp', 'op', 'av', 'hp', 'lp')),
+          "http://www.nasdaqomxnordic.com/bonds/denmark/microsite?Instrument=#{id}",
           Date.today
         )
       end
+      papers
     end
 
-    USER_AGENT = "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0"
+    USER_AGENT = 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0'
     def self.bonds(instruments)
-      inst_ids = instruments.map { |i| i.strip }.join(",")
+      inst_ids = instruments.map(&:strip).join(',')
       uri = URI.parse "http://www.nasdaqomxnordic.com/webproxy/DataFeedProxy.aspx?SubSystem=Prices&Action=GetInstrument&Instrument=#{inst_ids}"
-      REXML::Document.new(uri.open("User-Agent" => USER_AGENT).read.to_s).root
+      REXML::Document.new(uri.open('User-Agent' => USER_AGENT).read.to_s).root
     end
   end
 end
